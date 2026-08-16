f85a6fa7ace5fd41e5d4d1b2519695b855fc2089
admtikendra0001-stack <adm.tikendra0001@gmail.com>
2026-08-16 23:05:54 +0530

    Fix asset paths in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 const CACHE='tikendra-cashbook-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
